8de3324186878a4b7c7c7fe42c89b0a8f9615d55
Olalekan <ogunmolu@kivasystems.com>
2016-05-17 11:27:17 -0400

    all in a class

--- src/sub.cpp
+++ src/sub.cpp
@@ -31,39 +31,69 @@ namespace nodes
   enum Mode { color = 0,  depth = 1,  ir = 2,   mono = 3 };
 }
 
-void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+class Receiver
 {
-  try
-  {
-    ROS_INFO("msg frame_id: %s", msg->header.frame_id.c_str());
-    ROS_INFO("msg step %u",  msg->step);
-    ROS_INFO("msg encoding %s", msg->encoding.c_str());
+  ros::NodeHandle nc;
+  image_transport::ImageTransport it;
+  image_transport::Subscriber sub;
 
-    if(imageFormat == "color_rect") //color
-      {  
-        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-        cv::circle(cv_ptr->image, cv::Point(30, 30), 10, CV_RGB(255,0,0));  //draw a circle on the circle image
-      }
-    else if(msg->encoding.c_str() == "16UC1") //ir or depth
-      {  
-        if(msg->header.frame_id.c_str() == "kinect2_rgb_optical_frame") //depth
-        {          
-          cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::TYPE_16UC1);
-        }
-        else if(msg->header.frame_id.c_str() == "kinect2_ir_optical_frame") //ir)
-        {          
-          cv_ptr= cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::TYPE_16UC1);
-        }
-      }
+  public:
+    Receiver(std::string topicName, std::string imageFormat)
+      : running(false), it(nc), topicName_(topicName), imageFormat_(imageFormat)
+    {         
+      const std::string basetopic = "/kinect2";
+      std::string subName    = basetopic + "/" + topicName_ + "/" + "image_" + imageFormat_  ;
+      sub = it.subscribe(subName, 1, &Receiver::imageCallback, this);
+      ROS_INFO_STREAM("Subscribing to: " << subName);
+      windowName = imageFormat_ + " viewer";
+      cv::namedWindow(windowName);      
+      cv::startWindowThread();
+    }
 
-    cv::imshow(windowName, cv_bridge::toCvShare(msg, msg->encoding.c_str())->image);
-    cv::waitKey(3);
-  }
-  catch (cv_bridge::Exception& e)
-  {
-    ROS_ERROR("cv_bridge exception: %s", e.what());
-  }
-}
+    ~Receiver()
+    {      
+      cv::destroyWindow(windowName);
+    }
+
+    void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+    {
+      try
+      {
+        ROS_INFO("msg frame_id: %s", msg->header.frame_id.c_str());
+        ROS_INFO("msg step %u",  msg->step);
+        ROS_INFO("msg encoding %s", msg->encoding.c_str());
+
+        if(imageFormat == "color_rect") //color
+          {  
+            cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+            cv::circle(cv_ptr->image, cv::Point(30, 30), 10, CV_RGB(255,0,0));  //draw a circle on the circle image
+          }
+        else if(msg->encoding.c_str() == "16UC1") //ir or depth
+          {  
+            if(msg->header.frame_id.c_str() == "kinect2_rgb_optical_frame") //depth
+            {          
+              cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::TYPE_16UC1);
+            }
+            else if(msg->header.frame_id.c_str() == "kinect2_ir_optical_frame") //ir)
+            {          
+              cv_ptr= cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::TYPE_16UC1);
+            }
+          }
+
+        cv::imshow(windowName, cv_bridge::toCvShare(msg, msg->encoding.c_str())->image);
+        cv::waitKey(3);
+      }
+      catch (cv_bridge::Exception& e)
+      {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+      }
+    }
+
+  private:
+    bool running;
+    std::string topicName_, imageFormat_;
+};
+
 
 int main(int argc, char **argv)
 {  
@@ -80,7 +110,7 @@ int main(int argc, char **argv)
     case nodes::mono :
       nodeName = "monoGrabber"; break;
     default :
-      ROS_INFO("Ouch, this ain't yo' lane. Take the next exit. Wrong nodename supplied' ");
+      ROS_INFO("Ouch! This ain't yo' lane. Take the next exit. Wrong nodename supplied' ");
   }
   ros::init(argc, argv, nodeName, ros::init_options::AnonymousName);
 
@@ -96,36 +126,16 @@ int main(int argc, char **argv)
       return 0;
     }
 
-  for(size_t i = 1; i < (size_t)argc; ++i)
-  {
-      
+  for(size_t i = 1; i < (size_t)argc; ++i)  
+  {      
       topicName = argv[1];
       imageFormat = argv[2]; 
   }
 
   assert(imageFormat == "ir" || imageFormat == "ir_rect" || imageFormat == "color" || imageFormat == "color_rect" || imageFormat == "depth" || \
                          imageFormat == "depth_rect" || imageFormat == "mono" ||imageFormat ==   "mono_rect");
-
-  bool running(false);
-
-
-  const std::string basetopic = "/kinect2";
-
-  std::string subName    = basetopic + "/" + topicName + "/" + "image_" + imageFormat  ;
-
-  ROS_INFO_STREAM("Subscribing to: " << subName);
-
-  windowName = imageFormat + " viewer";
-
-
-  ros::NodeHandle nc;
-  cv::namedWindow(windowName);
-  cv::startWindowThread();
-  image_transport::ImageTransport it(nc);
-  image_transport::Subscriber sub = it.subscribe(subName, 1, imageCallback);
+  Receiver receiver(topicName, imageFormat);
 
   ros::spin();
-  cv::destroyWindow(windowName);
-
   return 0;
 }
